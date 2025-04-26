40e1796c99daf842872938d29f9bf8761f1f8480
Aitor-BG <aitorprg12@outlook.es>
2025-04-26 12:16:48 +0200

    v3.1_spatie/laravel-activitylog

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = ['name','start_date','end_date','color','details','inscritos','participantes'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly(['name','start_date','end_date','inscritos']);
+    }
 }
